3031273ec3463a2999fb179fc4da65dad7718f4c
Vitor S Rodrigues <vitor.rodrigues@gmail.com>
2017-09-18 21:09:16 -0300

    Implementedmiddleware for token verify

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,6 @@
 
 const restify = require('restify')
+const jwt = require('jsonwebtoken')
 const server = restify.createServer()
 const routes = require('../http/routes')
 const cors = require('./cors')
@@ -7,6 +8,22 @@ const cors = require('./cors')
 server.pre(cors.preflight)
 server.use(cors.actual)
 server.use(restify.plugins.bodyParser())
+server.use(async (req, res, next) => {
+  const token = req.headers['x-access-token']
+  if (!token) {
+    res.send(403, { error: 'Token não fornecido' })
+    return false
+  }
+  await jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
+    if (error) {
+      res.send(403, { error: 'Falha ao autenticar o token' })
+    } else {
+      req.decoded = decoded
+    }
+  })
+
+  next()
+})
 
 routes(server)
 
